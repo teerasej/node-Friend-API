3ed8c57fd637324218670bcd5276399d411d88a0
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-04-25 18:50:13 +0700

    define routes for work with friends data

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,25 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World!')
 });
+
+app.get('/friends', (req, res) => {
+    res.json({})
+})
+
+app.post('/friends', (req, res) => {
+    const newFriend = req.body
+    res.json(newFriend)
+})
+
+app.delete('/friends', (req, res) => {
+    const deletingFriend = req.body
+    res.json(deletingFriend)
+})
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
