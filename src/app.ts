0a82fe6e1f9e502a7e519f8de88ba864383b16b8
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-04-26 21:22:55 +0700

    setup passport to express

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose'
+import passportRouter from './config/passport'
 const app = express();
 const port = 3000;
 
 mongoose.connect('mongodb://localhost:27017/myapp');
 app.use(express.json())
 app.use(express.static('public'))
+app.use(passportRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
