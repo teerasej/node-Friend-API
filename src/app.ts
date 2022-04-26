7faaa80918185207772d6b1308f66d7e61f28894
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-04-26 21:22:35 +0700

    connect mongoose instance to mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import mongoose from 'mongoose'
 const app = express();
 const port = 3000;
 
+mongoose.connect('mongodb://localhost:27017/myapp');
 app.use(express.json())
 app.use(express.static('public'))
 
